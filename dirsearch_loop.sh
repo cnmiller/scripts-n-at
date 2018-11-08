b78aee43c3c471a386eb41aaff1051a64e05a933
Chase Miller <chasemiller5@gmail.com>
2018-11-08 09:08:57 -0500

    Update dirsearch_loop.sh

--- dirsearch_loop.sh
+++ dirsearch_loop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# A simple script to make Diresearch loop over a file containing URLs. For example, handy to use with GnmapParser's PeepingTom.txt.
+# Usage ./dirsearch_loop.sh [File Path]
 
 while read p; do
-	./dirsearch.py -u $p -e /
+	./dirsearch.py -u $p -e / #More options can be added here such as wordlists (-w), response code excludes (-x), etc.
 done <"$1"
